b3fe98c0f240a93e85607c434ba884c868bbb5f8
Guillaume <guillaumeloux@gmail.com>
2019-11-26 11:39:45 +0100

    Update destruction.c

--- sources/destruction.c
+++ sources/destruction.c
@@ -7,43 +7,71 @@
 
 int main( int argc, char* argv[]){
     
-    key_t cleSemaphore = ftok(".",1);
+    int nbSemaphores = 0;
+    int nbMemoires = 0;
 
-    if( cleSemaphore == -1 ){
-        perror("Erreur clé Semaphore\n");
-        return -1;
+    int condition = 1;
+
+    int cpt = 1;
+
+    while( condition ){
+
+        key_t cleSemaphore = ftok(".",cpt);
+
+        if( cleSemaphore == -1 ){
+            perror("Erreur clé Semaphore\n");
+            return -1;
+        }
+
+        int idSemaphore = semget(cleSemaphore,0,0666);
+
+        if( idSemaphore == -1 ){
+            condition = 0;
+        }
+
+        if( semctl(idSemaphore,0,IPC_RMID) == -1 ){
+            condition = 0;
+        }else{
+            nbSemaphores += 1;
+            cpt++;
+        }
+        
+
+    }
+    
+    condition = 1;
+
+    cpt = 10;
+
+    while ( condition )
+    {
+        key_t cleSegment = ftok(".",cpt);
+
+        if( cleSegment == -1 ){
+            perror("Erreur clé Segement\n");
+            return -1;
+        }
+
+        int idSegment = shmget(cleSegment,sizeof(int),IPC_CREAT|0666);
+
+        if( idSegment == -1 ){
+            condition = 0;
+        }
+
+        if( shmctl(idSegment,IPC_RMID,NULL) == -1 ){
+            condition = 0;
+        }else
+        {
+            nbMemoires += 1;
+            cpt++;
+        }
+        
     }
 
-    int idSemaphore = semget(cleSemaphore,0,0666);
+    printf("Nombre de semaphore détruite = %d.\n",nbSemaphores);
 
-    if( idSemaphore == -1 ){
-        perror("Erreur id Sémaphore\n");
-        return -1;
-    }
-
-    if( semctl(idSemaphore,0,IPC_RMID) == -1 ){
-        perror("Erreur destruction Sémaphore.\n");
-        return -1;
-    }
-
-    key_t cleSegment = ftok(".",2);
-
-    if( cleSegment == -1 ){
-        perror("Erreur clé Segement\n");
-        return -1;
-    }
-
-    int idSegment = shmget(cleSegment,sizeof(int),IPC_CREAT|0666);
-
-    if( idSegment == -1 ){
-        perror("Erreur id Segement\b");
-        return -1;
-    }
-
-    if( shmctl(idSegment,IPC_RMID,NULL) == -1 ){
-        perror("Erreur destruction Mémoire partagées?\n");
-    }
-
+    printf("Nombre de segment de mémoire dértuit = %d.\n",nbMemoires);
+    
 
     return 0;
 }
